decce584ef106b9b3c323d952ce2097438716c57
realnotben <benjaminfigueroa022@gmail.com>
2026-01-16 10:30:48 -0800

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by FIGUEROA, BENJAMIN on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
